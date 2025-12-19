cfc31196a92fe1a69ab40fa6f6efdf4d5d2bb179
Adal Andrade <aaandrade5@gmail.com>
2025-12-19 12:51:51 -0600

    remove registry

--- p33k-challenge/setup-kind-cluster.sh
+++ p33k-challenge/setup-kind-cluster.sh
@@ -6,8 +6,5 @@ set -e
 
 kind create cluster --name voting-platform --config kind-config.yaml || echo "Cluster already exists, skipping creation..."
 
-
-docker rm -f kind-registry registry 2>/dev/null || echo "No old registry containers found, skipping..."
-
 echo "Kind cluster 'voting-platform' is ready."
 echo "Now use the 'build-and-load.sh' script to push your images."
